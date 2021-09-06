73c0efcc13005215f1fff74404bb8704f2db4080
Jhon <80256066+nakajima-john-shotaro@users.noreply.github.com>
2021-09-06 12:15:11 +0900

    [WIP] First page almost done.

--- aicon/frontend/aicon.js
+++ aicon/frontend/aicon.js
@@ -4,3 +4,24 @@ $(document).ready(function(){
         responsiveThreshold : 1920
     });
 });
+
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+});
+
+$(document).ready(function(){
+    $('.carousel').carousel();
+});
+
+$(document).ready(function(){
+$('select').formSelect();
+});
+
+$(function () {
+    $('.cancelEnter')
+        .on('keydown', function (e) {
+            if (e.key == 'Enter') {
+                return false;
+            }
+        })
+});
---
+++ aicon/frontend/aicon_function.js
@@ -0,0 +1,7 @@
+function ClearTextArea() {
+    $("#ja_text").val("");
+}
+
+function reload(){
+    window.location.reload();
+}
