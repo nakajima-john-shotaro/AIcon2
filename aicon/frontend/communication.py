f740fca3b54cfcc5e0bfdfceb4a69265df99a7d5
Jhon <80256066+nakajima-john-shotaro@users.noreply.github.com>
2021-09-07 20:43:41 +0900

    [add] Create communication file

---
+++ aicon/frontend/communication.py
@@ -0,0 +1,15 @@
+from logging import debug
+from flask import render_template, Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    # return "Hello World!"
+    return render_template("aicon.html", title="AIcon", name="AIcon")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8083, threaded=True)
