447bb14356a110c09347cd88c41fde5bd11fa332
urasaki keisuke <213427006@ccmailg.meijo-u.ac.jp>
2021-09-07 18:28:14 +0900

    [update] wip

---
+++ aicon/backend/constant.py
@@ -0,0 +1,16 @@
+"""constant.py"""
+
+JSON_MODEL_NAME: str = "model_name"
+JSON_TEXT: str = "text"
+JSON_CURRENT_ITER: str = "current_iter"
+JSON_TOTAL_ITER: str = "total_iter"
+JSON_SIZE: str = "size"
+JSON_HASH: str = "hash"
+JSON_COMPLETE: str = "complete"
+JSON_ABORT: str = "abort"
+JSON_PRIORITY: str = "priority"
+JSON_NUM_CLIENTS: str = "num_clients"
+JSON_IMG_PATH: str = "img_path"
+JSON_GIF_PATH: str = "git_path"
+
+P_HASH_INIT: str = "00000000-0000-0000-0000-000000000000"
--- aicon/backend/runme.py
+++ aicon/backend/runme.py
@@ -1,16 +1,28 @@
+import os
 import time
 import json
+from typing import Any, Dict, List, Optional, Union
 import urllib
 import requests
+from uuid import uuid4 
+from queue import Queue
 
-import chromedriver_binary
-import pyperclip
+import chromedriver_binary # pylint: disable=unused-import
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from flask import Flask, jsonify
+from flask import Flask, Response, jsonify, make_response, abort, request
 from flask_cors import CORS
-from bs4 import BeautifulSoup
+from werkzeug.exceptions import HTTPException, BadRequest, Forbidden
+from waitress import serve
 from googletrans import Translator
+
+from .cp import *
+
+
+app: Flask = Flask(__name__)
+app.config["JSON_AS_ASCII"] = False
+app.config["JSON_SORT_KEYS"] = False
+CORS(app)
 
 
 class Translation(object):
@@ -63,22 +75,118 @@ class Translation(object):
 
 class AIcon(object):
     def __init__(
-        self
+        self,
+        translator: str = "deepl",
     ) -> None:
         super().__init__()
 
-        app: Flask = Flask(__name__)
-        app.config["JSON_AS_ASCII"] = False
-        app.config["JSON_SORT_KEYS"] = False
-        CORS(app)
+        self.translator: Translation = Translation(translator)
 
-        self.translator: Translation = Translation("deepl")
+        self.client_data: Dict[str, Dict[str, str]] = {}
 
+        self.base_img_path: str = "../frontend/static/dst_img"
+        self.base_gif_path: str = "../frontend/static/dst_gif"
 
-# def main() -> None:
-#     app.run(host="0.0.0.0", port=8081, threaded=False)
+    def _get_client_priority(self, client_hash: str) -> int:
+        return list(self.client_data.keys()).index(client_hash)
+
+    def _set_path(self, client_hash: str) -> None:
+        model_name: str = self.client_data[client_hash][JSON_MODEL_NAME]
+
+        img_path: str = os.path.join(self.base_img_path, model_name, client_hash)
+        gif_path: str = os.path.join(self.base_gif_path, model_name, client_hash)
+
+        os.makedirs(img_path, exist_ok=True)
+        os.makedirs(gif_path, exist_ok=True)
+
+        self.client_data[client_hash][JSON_IMG_PATH] = img_path
+        self.client_data[client_hash][JSON_GIF_PATH] = gif_path
+        
+
+    @app.route("/", methods=["POST"])
+    def main(self):
+        recieved_data: Dict[str, Any] = request.get_json()
+        print(recieved_data)
+
+        client_hash: str = recieved_data[JSON_HASH]
+
+        if client_hash == P_HASH_INIT:
+            client_hash = str(uuid4())
+
+            try:
+                self.client_data[client_hash] = {
+                    JSON_MODEL_NAME: recieved_data[JSON_MODEL_NAME],
+                    JSON_TEXT: self.translator(recieved_data[JSON_TEXT]),
+                    JSON_TOTAL_ITER: recieved_data[JSON_TOTAL_ITER],
+                    JSON_SIZE: recieved_data[JSON_SIZE],
+                    JSON_COMPLETE: False,
+                }
+            except KeyError as error_state:
+                abort(400, error_state)
+
+            self._set_path(client_hash)
+
+            res: Dict[str, Optional[Union[str, bool]]] = {
+                JSON_HASH: client_hash,
+                JSON_PRIORITY: self._get_client_priority(client_hash),
+                JSON_NUM_CLIENTS: len(self.client_data),
+                JSON_CURRENT_ITER: "0",
+                JSON_COMPLETE: False,
+                JSON_IMG_PATH: None,
+                JSON_GIF_PATH: None,
+            }
+
+            return jsonify(res)
+
+        if client_hash in self.client_data.keys():
+            self.client_data[client_hash][JSON_ABORT] = recieved_data[JSON_ABORT]
+
+            client_priority: int = self._get_client_priority(client_hash)
+            if client_priority == 0:
+                client_data: Dict[str, str] = self.client_data[client_hash]
+
+                # Do something here
+
+                res: Dict[str, Optional[Union[str, bool]]] = {
+                    JSON_HASH: client_hash,
+                    JSON_PRIORITY: client_priority,
+                    JSON_NUM_CLIENTS: len(self.client_data),
+                    JSON_CURRENT_ITER: "0",
+                    JSON_COMPLETE: False,
+                    JSON_IMG_PATH: None,
+                    JSON_GIF_PATH: None,
+                }
+            else:
+                res: Dict[str, Optional[Union[str, bool]]] = {
+                    JSON_HASH: client_hash,
+                    JSON_PRIORITY: client_priority,
+                    JSON_NUM_CLIENTS: len(self.client_data),
+                    JSON_CURRENT_ITER: "0",
+                    JSON_COMPLETE: False,
+                    JSON_IMG_PATH: None,
+                    JSON_GIF_PATH: None,
+                }
+        else:
+            abort(Forbidden, f"Invalid Hash: {client_hash}")
+
+        return jsonify(res)
+    
+    @app.errorhandler(BadRequest)
+    @app.errorhandler(Forbidden)
+    def handle_exception(e: HTTPException):
+        """Return JSON instead of HTML for HTTP errors."""
+        response: Response = e.get_response()
+        response.data = json.dumps({
+            "code": e.code,
+            "name": e.name,
+            "description": e.description,
+        })
+        response.content_type = "application/json"
+
+        return response
 
 
 if __name__ == "__main__":
-    translator = Translation("deepl")
-    translator.translate("東京特許許可局")
+    aicon: AIcon = AIcon()
+
+    serve(app, host='0.0.0.0', port=8081, threads=1)
