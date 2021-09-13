a6100d1fd7e90870d18fb4be760e2d1b87ec0fc5
urasaki keisuke <213427006@ccmailg.meijo-u.ac.jp>
2021-09-13 16:53:18 +0900

    [test] Fixed import

--- aicon/test/backend_import.py
+++ aicon/test/backend_import.py
@@ -2,15 +2,15 @@
 
 # pylint: disable=unused-import
 
-import time
-import json
-import urllib
-import requests
-
-import chromedriver_binary
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from flask import Flask, jsonify
-from flask_cors import CORS
 from bs4 import BeautifulSoup
 from googletrans import Translator
+from flask import Flask, Response, jsonify, render_template, abort, request
+from flask_cors import CORS
+from flask_restful import Api, Resource
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from werkzeug.exceptions import HTTPException, BadRequest, Forbidden, InternalServerError
+from waitress import serve
