48fed803422cef67d34b863dda71c8d04c79d9dd
Jhon <80256066+nakajima-john-shotaro@users.noreply.github.com>
2021-09-15 12:46:29 +0900

    [change] clean up code

--- aicon/frontend/static/js/constant.js
+++ aicon/frontend/static/js/constant.js
@@ -1,46 +1,45 @@
 var MODEL_PARAM = {
-    "DeepDaze": {
-        "low": {
-            "iter": 200,
-            "num_layer": 24,
-            "hidden_size": 256,
-            "batch_size": 1,
-            "gae": 1,
-            "backbone": "RN50"
+    DeepDaze: {
+        low: {
+            iter: 400,
+            num_layer: 24,
+            hidden_size: 256,
+            batch_size: 1,
+            gae: 1,
+            backbone: 'RN101'
         },
-        "middle": {
-            "iter": 500,
-            "num_layer": 24,
-            "hidden_size": 512,
-            "batch_size": 1,
-            "gae": 1,
-            "backbone": "RN50x4"
+        middle: {
+            iter: 800,
+            num_layer: 24,
+            hidden_size: 512,
+            batch_size: 2,
+            gae: 1,
+            backbone: 'ViT-B32'
         },
-        "high": {
-            "iter": 1000,
-            "num_layer": 24,
-            "hidden_size": 512,
-            "batch_size": 4,
-            "gae": 1,
-            "backbone": "ViT-B32"
-        }
+        high: {
+            iter: 1000,
+            num_layer: 24,
+            hidden_size: 1024,
+            batch_size: 4,
+            gae: 1,
+            backbone: 'ViT-B32'
+        },
     },
-    "BigSleep": {
-        "low": {
-            "iter": 60,
-            "gae": 1,
-            "backbone": "ViT-B32"
+    BigSleep: {
+        low: {
+            iter: 600,
+            gae: 1,
+            backbone: 'RN50x4'
         },
-        "middle": {
-            "iter": 100,
-            "gae": 1,
-            "backbone": "RN50x4"
+        middle: {
+            iter: 800,
+            gae: 1,
+            backbone: 'ViT-B32'
         },
-        "high": {
-            "iter": 150,
-            "gae": 1,
-            "backbone": "RN50"
-        }
+        high: {
+            iter: 1000,
+            gae: 1,
+            backbone: 'ViT-B32'
+        },
     }
 }
-
