3eae26f6b2ffd64e180181680b5fdd5db126ee0c
urasaki keisuke <213427006@ccmailg.meijo-u.ac.jp>
2021-09-17 20:28:27 +0900

    [change] Changed some functions

--- aicon/backend/twitter.py
+++ aicon/backend/twitter.py
@@ -1,9 +1,8 @@
 import os
-from urllib.parse import unquote
 from typing import Dict, Optional, Union
 
 from tweepy import OAuthHandler, API, TweepError
-from flask import Response, make_response, jsonify, render_template, request
+from flask import Response, make_response, jsonify
 from flask_restful import Resource
 from constant import *
 
@@ -52,57 +51,3 @@ class Authorization(Resource):
 
         finally:
             return make_response(jsonify(res))
-    
-
-class Callback(Resource):
-    def get(self) -> Response:
-        oauth_token = request.args.get(TWITTER_OAUTH_TOKEN)
-        oauth_verifier = request.args.get(TWITTER_OAUTH_VERIFIER)
-
-        if oauth_token is None:
-            logger.error(f"<<AIcon Twitter Control>> Cannot get `oauth_token`")
-        
-        if  oauth_verifier is None:
-            logger.error(f"<<AIcon Twitter Control>> Cannot get `oauth_verifier`")
-
-        return render_template("twitter-callback.html", oauth_token=oauth_token, oauth_verifier=oauth_verifier)
-
-
-class Executor(Resource):
-    def get(self) -> Response:
-        oauth_token = request.args.get(TWITTER_OAUTH_TOKEN)
-        oauth_verifier = request.args.get(TWITTER_OAUTH_VERIFIER)
-
-        try:
-            secrets: Dict[str, Optional[str]] = get_secrets()
-
-            auth_handler: OAuthHandler = OAuthHandler(
-                consumer_key=secrets[TWITTER_CONSUMER_KEY],
-                consumer_secret=secrets[TWITTER_CONSUMER_SECRET]
-            )
-
-            auth_handler.request_token = {
-                TWITTER_OAUTH_TOKEN: oauth_token,
-                TWITTER_OAUTH_TOKEN_SECRET: oauth_verifier
-            }
-
-            auth_handler.get_access_token(oauth_verifier)
-
-            img_path: str = request.cookies.get(TWITTER_IMG_PATH)
-            mode: str = request.cookies.get(TWITTER_MODE)
-
-            api: API = API(auth_handler=auth_handler)
-
-            if mode == TWITTER_MODE_ICON:
-                img_path = unquote(img_path)
-                api.update_profile_image(img_path)
-
-            elif mode == TWITTER_MODE_TWEET:
-                img_path = unquote(img_path)
-                api.update_with_media(status="AIconでアイコンを作ったよ！！\n\n#技育展\n#AIcon", filename=img_path)
-
-            return render_template("twitter-send.html", title="Twitter-Send", name="Twitter-Send")
-
-
-        except AIconEnvVarNotFindError as e:
-            logger.error(f"<<AIcon Twitter Control>> {e}")
